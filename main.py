d4356d57d2c749cbebcc13250fe5cb6661ec63d0
tomy08 <tomasesantacruz@gmail.com>
2024-03-25 18:43:35 -0300

    add minnor changes

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
 from routers.books import book_router
 from routers.user import user_router
-from db.database import create_database
+from db.database import SessionLocal, create_database
+from services.crud import get_books
 
 
 app = FastAPI()
@@ -14,3 +17,7 @@ app.include_router(book_router)
 app.include_router(user_router)
 
 
+@app.get("/",tags=["home"], response_class=RedirectResponse)
+async def home():
+    return RedirectResponse(url="/docs")
+    
